485baee39294365ba2a5d1dde6649ec789563ab1
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-08-28 15:49:52 +0300

    Make registary file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
-import "./globals.css";
 import { Inter } from "next/font/google";
+import StyledComponentsRegistry from "./lib/registry";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
