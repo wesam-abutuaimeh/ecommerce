3dc6864566617d4106e46f6dffbefaee8506b065
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-09-02 14:22:37 +0300

    Chip component

---
+++ src/components/atoms/Chip/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { Chip } from './style'
+
+const index = ({ content }) => <Chip>{content}</Chip>
+export default index
---
+++ src/components/atoms/Chip/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Chip = styled.span`
+  background: #ffe3e3;
+  height: 28px;
+  padding: 3px 13px 4px 13px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  display: inline-flex;
+  color: #eb001b;
+  font-size: 14px;
+  font-weight: 500;
+  border-radius: 29px;
+  letter-spacing: -0.2px;
+`;
