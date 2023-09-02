dfcf03822c00f969f13c690d0d3ba2d0511a22cc
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-09-02 14:27:51 +0300

    Services component

---
+++ src/components/sections/Services/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { StyledFlex } from "@/styles/commen";
+
+export const ExtraServices = styled.div`
+  margin-top: 15px;
+`;
+
+export const Flex = styled(StyledFlex)``;
