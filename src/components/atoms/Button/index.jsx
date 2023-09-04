c8b8b109f643973fb4ceb07c8e17f8f1f9f986d7
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-09-04 19:44:57 +0300

    add onClick prop

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyledButton } from './style';
-const Button = ({ children, bgColor, color, width, height, padding, }) =>
-    <StyledButton style={{ backgroundColor: bgColor, color: color, width: width, height: height, padding: padding }}>{children}</StyledButton>;
+const Button = ({ children, bgColor, color, width, height, padding, onClick, type }) =>
+    <StyledButton style={{ backgroundColor: bgColor, color: color, width: width, height: height, padding: padding }}
+        type={type} onClick={onClick}
+    >{children}</StyledButton>;
 export default Button;
