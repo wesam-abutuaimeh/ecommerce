e3625f2a4e7f5f15222bda742aed51d2357fa92d
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-09-04 19:45:22 +0300

    add onChange prop

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,29 @@
-import styles from "./page.module.css";
+import HeroSection from "@/components/sections/HeroSection";
+import DealsAndOffersSection from "@/components/sections/DealsAndOffersSection/";
+import RecommendedItems from "@/components/sections/RecommendedItems";
+import Services from "@/components/sections/Services/";
+import Regions from "@/components/sections/Regions";
+import Subscription from "@/components/sections/Subscription";
+import Contact from "@/components/sections/Contact";
+import HomeAndOutdoor from "@/components/sections/HomeAndOutdoor";
+import ConsumerElectronicsAndGadgets from "@/components/sections/ConsumerElectronicsAndGadgets";
+import Header from "@/components/organisms/Header";
+import Footer from "@/components/organisms/Footer";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Learn Next.js</h1>
+    <main>
+      <Header />
+      <HeroSection />
+      <DealsAndOffersSection />
+      <HomeAndOutdoor />
+      <ConsumerElectronicsAndGadgets />
+      <Contact />
+      <RecommendedItems />
+      <Services />
+      <Regions />
+      <Subscription />
+      <Footer />
     </main>
   );
 }
